e4d4dbe08f1a60fb9f6a4874a95a43b04c0cb13f
Feyza GAZIOGLU <feyza.gazioglu8@gmail.com>
2025-11-13 19:54:49 +0300

    to solve some mistakes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,10 @@
+'use client';
 import Link from 'next/link';
+import { useState } from 'react';
 
 export const Navbar = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
     <nav className='flex gap-4 justify-end p-4 bg-amber-950 text-slate-200'>
       <Link href='/'>Home</Link>
